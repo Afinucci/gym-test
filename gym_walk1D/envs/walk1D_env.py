48b6664a11cbe68111463c6f6920356a7b4f7c9d
Afinucci <30832019+Afinucci@users.noreply.github.com>
2022-07-20 15:38:23 +0200

    Update walk1D_env.py

--- gym_walk1D/envs/walk1D_env.py
+++ gym_walk1D/envs/walk1D_env.py
@@ -1,15 +1,38 @@
 import gym
 from gym import error, spaces, utils
+import numpy as np
 from gym.utils import seeding
 
 class walk1DEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
-    ...
+    self.action_space = spaces.Discrete(3) # Right=1; Left=-1; Do Nothing=0
+    self.observation_space = np.array([-1,-1,0,1,1])  # Environment: (-1,-1,0,1,1)
+  
   def step(self, action):
-    ...
+    # remember that the state is the index of the vector
+    if action == 1: #moving right
+      state += action
+      reward = observation_space[state]
+    elif action == -1: # moving left
+      state += action
+      reward = observation_space[state]
+    elif action == 0: # not moving
+      state += action
+      reward = observation_space[state]
+    
+    if reward == 2:
+      done = True
+      info = {}
+    elif reward == -1:
+      done = True
+      info = {}
+    return state, reward, done, info
   def reset(self):
-    ...
+    state = 2 # the state represent also the index of the vector that in turn is the environment
+    return state
   def render(self, mode='human', close=False):
-    ...
+    environment = np.zeros(5,)
+    environment[state] = reward
+    return environment
