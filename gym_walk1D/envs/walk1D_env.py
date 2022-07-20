581e0d8aed4d379cdee5054e180b4837a82fae9b
Afinucci <30832019+Afinucci@users.noreply.github.com>
2022-07-20 16:36:17 +0200

    Update walk1D_env.py

--- gym_walk1D/envs/walk1D_env.py
+++ gym_walk1D/envs/walk1D_env.py
@@ -61,29 +61,13 @@ class walk1DEnv(gym.Env):
 
     return np.array([self.agent_pos]).astype(np.float32), reward, done, info
   
-  def step(self, action):
-    
-    if action == 2: #moving right
-      self.state += 1
-      reward = observation_space[state]
-    elif action == 1: # moving left
-      self.state += -1
-      reward = observation_space[state]
-    elif action == 0: # not moving
-      self.state += 0
-      reward = observation_space[state]
-    
-    if reward == 2:
-      done = True
-      info = {}
-    elif reward == -1:
-      done = True
-      info = {}
-    return state, reward, done, info
-  def reset(self):
-    state = 2 # the state represent also the index of the vector that in turn is the environment
-    return state
-  def render(self, mode='human', close=False):
-    environment = np.zeros(5,)
-    environment[state] = reward
-    return environment
+  def render(self, mode='console'):
+    if mode != 'console':
+      raise NotImplementedError()
+    # agent is represented as a cross, rest as a dot
+    print("." * self.agent_pos, end="")
+    print("x", end="")
+    print("." * (self.grid_size - self.agent_pos))
+
+  def close(self):
+    pass
